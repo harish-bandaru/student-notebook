e184a72d014cbcc50732d017153a8664a19483a2
harishbabu.bandaru <harishbabu.bandaru1995@gmail.com>
2022-11-04 16:21:16 -0400

    added java script file

---
+++ script_js.js
@@ -0,0 +1,118 @@
+const login = document.getElementById("loginForm");
+const register = document.getElementById("regForm");
+const note = document.getElementById("noteForm")
+
+if(login) login.addEventListener('submit',loginpageFunction)
+if(register) register.addEventListener('submit',registerpageFunction)
+if(note) note.addEventListener('submit',notepageFunction)
+
+function loginpageFunction(e)
+{
+    e.preventDefault();
+    let username=document.getElementById('username').value;
+    let password=document.getElementById('passwd').value;
+
+
+    class User{
+        constructor(username,password)
+        {
+            this.uname=username;
+            this.pword=password;
+        }
+    
+    
+    
+    getuname(){
+        return this.uname;
+    }
+    setuname(newusername){
+        this.uname = username;
+    }
+    getpword(){
+        return this.pword;
+    }
+    setpword(newpassword){
+        this.pword = password
+    }
+
+    }
+
+    const Userl=new User(username,password);
+    console.log(Userl);
+
+}
+
+
+function registerpageFunction(e)
+{
+    e.preventDefault()
+    let fname=document.getElementById('fname').value;
+    let lname=document.getElementById('lname').value;
+    let email=document.getElementById('email').value;
+    let password=document.getElementById('passwd').value;
+
+    class User{
+        constructor(fname,lname,email,password)
+        {
+            this.firstname=fname;
+            this.lastname=lname;
+            this.email=email;
+            this.password=password;
+        }
+        getemail(){
+            return this.email;
+        }
+        setemail(newemail){
+            this.email = newemail;
+        }
+        getpassword(){
+            return this.password;
+        }
+        setpassword(newpassword){
+            this.password=newpassword
+        }
+        getfirstname(){
+            return this.firstname;
+        }
+        setfirstname(newfirstname){
+            this.firstname = newfirstname;
+        }
+        getlastname(){
+            return this.lastname;
+        }
+        setlastname(newlastname){
+            this.lastname=newlastname;
+        }
+    }
+
+    const user1=new User(fname,lname,email,password);
+    console.log(user1);
+}
+
+
+function notepageFunction(e)
+{
+    e.preventDefault();
+    let note=document.getElementById('note').value;
+
+    class User{
+        constructor(note)
+        {
+            this.tnotes=note;
+        }
+    
+    
+    gettnotes(){
+        return this.tnotes;
+    }
+    settnotes(note){
+        this.tnotes = note;
+    }
+   
+
+    }
+
+    const Userl=new User(note);
+    console.log(Userl);
+
+}
